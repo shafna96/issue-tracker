928bcdf0d6c1b771dd8b4f4e6d6acae23d7b7db5
shafna96 <159750001+shafna-Ascension@users.noreply.github.com>
2024-04-11 15:48:04 +0530

    Refactor: Organize import statements

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,10 @@
-import React from "react";
-import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import Link from "../components/Link";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
+import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
+import { IssueStatusBadge, Link } from "@/app/components";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
 
   return (
     <div>
